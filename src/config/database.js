d35899395f731e71e53b4396c501113f0eb52835
Vinicius de Moraes <vinicius.carvalho@ernetwork.com.br>
2019-11-26 17:54:24 -0300

    First commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'root',
+  password: 'ernetwork',
+  database: 'nodeauth',
+  dialect: 'mysql',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
